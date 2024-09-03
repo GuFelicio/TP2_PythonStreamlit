2c2c5ef9d00f54c415d02b9378d272624f30962d
Gustavo Felício <gu.felicio10@gmail.com>
2024-09-03 19:47:34 -0300

    Alteração para puxar o CSV com edição e conseguir fazer a leitura do CSV!

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import plotly.graph_objects as go
 st.markdown("**Gráfico de Barras: Evolução Semanal dos Casos Novos de COVID-19 em São Paulo**")
 
 # Carregar o arquivo CSV com o delimitador correto
-file_path = '/mnt/data/HIST_PAINEL_COVIDBR_2020_Parte1_30ago2024.csv'
+file_path = r'D:\Faculdade\2 Semestre\Python + Front\TP2\HIST_PAINEL_COVIDBR_2020_Parte1_30ago2024.csv'
 data = pd.read_csv(file_path, sep=';', on_bad_lines='skip')
 
 # Verificar os nomes das colunas
